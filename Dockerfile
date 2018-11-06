5536b13904a39356cc27fde1f8d9256dc6a4f2e5
Anton Vlasik <snumber29@gmail.com>
2018-11-06 13:15:57 +0300

    added configs

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-# In your Dockerfile.
-FROM node:latest
-MAINTAINER Anton Vlasik
-ENV PORT=3020
-COPY . .
-RUN npm install
-EXPOSE $PORT
-CMD npm run build
--- src/components/AdjacentProduction/AdjacentCalc.jsx
+++ src/components/AdjacentProduction/AdjacentCalc.jsx
@@ -3,24 +3,6 @@ import classNames from 'classnames';
 import {matrix, findMaxAdjacent, getRandomValue} from './helper';
 
 class AdjacentCalc extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.handleLimitChange = this.handleLimitChange.bind(this);
-    this.handleChangeSize = this.handleChangeSize.bind(this);
-    this.sizeLimit = 20;
-    this.defaultLimit = 4;
-    this.state = {
-      isLimitInvalid: false,
-      isSizeInvalid: false,
-      limit: this.defaultLimit,
-      size: 20,
-      data: matrix,
-      result: findMaxAdjacent(matrix, this.defaultLimit)
-    };
-  }
-
   static populateData(size) {
     const data = [];
     if (!size) return [[]];
@@ -33,40 +15,91 @@ class AdjacentCalc extends Component {
     return data;
   }
 
+  static getInitialState(config) {
+    const initialState = {
+      validation: {},
+      values: {},
+      data: matrix,
+      result: findMaxAdjacent(matrix, config.find(item => item.id === 'limit').defaultValue)
+    };
+    config.forEach(item => {
+      initialState.validation[item.id] = false;
+      initialState.values[item.id] = item.defaultValue;
+    });
+    return initialState;
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.handleLimitChange = this.handleLimitChange.bind(this);
+    this.handleChangeSize = this.handleChangeSize.bind(this);
+    this.regenerate = this.regenerate.bind(this);
+    this.defaultLimit = 20;
+    this.config = [
+      {
+        id: 'size',
+        defaultValue: this.defaultLimit,
+        onChange: this.handleChangeSize,
+        getLimitRule: () => this.defaultLimit
+      },
+      {
+        id: 'limit',
+        defaultValue: 4,
+        onChange: this.handleLimitChange,
+        getLimitRule: () => this.state.size
+      }
+    ];
+    this.state = AdjacentCalc.getInitialState(this.config);
+  }
+
   handleLimitChange({target: {value}}) {
     const isLimitInvalid = +value > this.state.size;
 
-    isLimitInvalid ? this.setState({
-      isLimitInvalid,
-      limit: value
-    }) : this.setState({
-      isLimitInvalid,
-      limit: value,
-      result: value
-        ? findMaxAdjacent(this.state.data, +value)
-        : {indexes: []}
-    });
+    if (isLimitInvalid) {
+      this.setState(({validation}) => ({
+        validation: {...validation, limit: isLimitInvalid},
+        limit: value
+      }));
+    } else {
+      this.setState(({validation}) => ({
+        validation: {...validation, limit: isLimitInvalid},
+        limit: value,
+        result: value
+          ? findMaxAdjacent(this.state.data, +value)
+          : {indexes: []}
+      }));
+    }
   }
 
   handleChangeSize({target: {value}}) {
     const isSizeInvalid = +value > this.sizeLimit;
     if (isSizeInvalid) {
-      this.setState({
-        isSizeInvalid
-      });
+      this.setState(({validation}) => ({
+        validation: {...validation, size: isSizeInvalid},
+        size: value
+      }));
     } else {
       const data = AdjacentCalc.populateData(+value);
-      this.setState({
-        isSizeInvalid,
+      this.setState(({validation}) => ({
+        validation: {...validation, size: isSizeInvalid},
         size: value,
         data,
         result: findMaxAdjacent(data, +this.state.limit)
-      });
+      }));
     }
   }
 
-  defineIfCellIsActive(rowInd, cellInd) {
-    return this.state.result.indexes.find(({x, y}) => rowInd === x && cellInd === y);
+  isCellActive(rowIndex, cellIndex) {
+    return this.state.result.indexes.find(({x, y}) => rowIndex === x && cellIndex === y);
+  }
+
+  regenerate() {
+    const data = AdjacentCalc.populateData(+this.state.size);
+    this.setState({
+      data,
+      result: findMaxAdjacent(data, +this.state.limit)
+    });
   }
 
   getData() {
@@ -81,7 +114,7 @@ class AdjacentCalc extends Component {
               key={cellIndex}
               className={classNames(
                 'adjacent-calc-cell',
-                {active: this.defineIfCellIsActive(rowIndex, cellIndex)}
+                {active: this.isCellActive(rowIndex, cellIndex)}
               )}
             >
               {cell}
@@ -95,33 +128,34 @@ class AdjacentCalc extends Component {
     return (
       <div className="adjacent-calc-container">
         <div className="controls-block">
-          <div className="control">
-            <label htmlFor="limit">Limit</label>
-            <div className="input-wrapper">
-              <input
-                id="limit"
-                type="number"
-                className={classNames({'has-error': this.state.isLimitInvalid})}
-                onChange={this.handleLimitChange}
-                value={this.state.limit}
-              />
-              {this.state.isLimitInvalid && <div className="error"> {`can't be more than ${this.state.size}`}</div>}
-            </div>
-          </div>
-          <div className="control">
-            <label htmlFor="size">Size</label>
-            <input
-              id="size"
-              type="number"
-              className={classNames({'has-error': this.state.isSizeInvalid})}
-              onChange={this.handleChangeSize}
-              value={this.state.size}
-            />
-            {this.state.isSizeInvalid && <div className="error"> {`can't be more than ${this.sizeLimit}`}</div>}
-          </div>
-          <div className="control">
-            <label>Result: </label>
-            <span>{this.state.result.res}</span>
+          <button
+            className="regenerate-button"
+            onClick={this.regenerate}
+          >
+            Regenerate Data
+          </button>
+          {this.config.map(({id, onChange, getLimitRule}) => {
+            return (
+              <div
+                className="control"
+                key={id}
+              >
+                <label htmlFor={id}>{id}</label>
+                <div className="input-wrapper">
+                  <input
+                    id={id}
+                    type="number"
+                    className={classNames({'has-error': this.state.validation[id]})}
+                    onChange={onChange}
+                    value={this.state[id]}
+                  />
+                  {this.state.validation[id] && <div className="error"> {`can't be more than ${getLimitRule()}`}</div>}
+                </div>
+              </div>
+            );
+          })}
+          <div className="result">
+            Result: {this.state.result.res}
           </div>
         </div>
         <div className="numbers-wrapper">
